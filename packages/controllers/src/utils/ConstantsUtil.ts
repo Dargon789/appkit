9796858d414cfffe182aa23ae38fe4a7fed0461b
AU_gdev_19 <64915515+Dargon789@users.noreply.github.com>
2025-05-18 10:44:58 +0700

    Revert 4281 chore/support more namespaces (#198)
    * Revert "chore: add cosmos namespace (#4281)"
    
    This reverts commit 30a9e1bbc5978a2cac63cf46af89aa3c34ad24c7.
    
    * Update examples/react-cloud-auth/src/components/SessionProvider.tsx
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: sourcery-ai[bot] <58596630+sourcery-ai[bot]@users.noreply.github.com>

--- packages/controllers/src/utils/ConstantsUtil.ts
+++ packages/controllers/src/utils/ConstantsUtil.ts
@@ -212,8 +212,7 @@ export const ConstantsUtil = {
     eip155: '0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee',
     solana: 'So11111111111111111111111111111111111111111',
     polkadot: '0x',
-    bip122: '0x',
-    cosmos: '0x'
+    bip122: '0x'
   } as const satisfies Record<ChainNamespace, string>,
 
   CONVERT_SLIPPAGE_TOLERANCE: 1,
