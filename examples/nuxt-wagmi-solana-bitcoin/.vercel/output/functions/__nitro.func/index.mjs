193df29481b2024055a18894cdaa8f79322573b7
AU_gdev_19 <64915515+Dargon789@users.noreply.github.com>
2025-05-23 08:53:15 +0700

    vc.build

---
+++ examples/nuxt-wagmi-solana-bitcoin/.vercel/output/functions/__nitro.func/index.mjs
@@ -0,0 +1,13 @@
+import 'node:buffer'
+import 'node:crypto'
+import 'node:events'
+import 'node:fs'
+import 'node:http'
+import 'node:https'
+import 'node:path'
+import process from 'node:process'
+
+globalThis._importMeta_ = { url: import.meta.url, env: process.env }
+export { l as default } from './chunks/nitro/nitro.mjs'
+
+//# sourceMappingURL=index.mjs.map
